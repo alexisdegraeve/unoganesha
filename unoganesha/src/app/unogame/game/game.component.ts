a9ada9f64df157221464c42223f24ca01d2568ea
Alexis Degraeve <alexis.degraeve@hotmail.com>
2022-05-28 10:56:29 +0200

    add game component

--- unoganesha/src/app/unogame/game/game.component.ts
+++ unoganesha/src/app/unogame/game/game.component.ts
@@ -9,12 +9,34 @@ import { FigureUno } from 'src/app/unocommon/Enum/figure';
 })
 export class GameComponent implements OnInit {
   colorUno: typeof ColorUno = ColorUno;
-  figureUno: typeof FigureUno = FigureUno;
+  figureUno: typeof FigureUno = FigureUno;  
+  gamecard: Array<number>=[];
 
-  
   constructor() { }
 
   ngOnInit(): void {
+    this.initGame();
   }
 
-}
+  initGame() {
+    this.initCards();
+    this.shuffleCards();
+  }
+
+  initCards(){
+    for (let index = 0; index < 108; index++) {
+      this.gamecard.push(index);
+    }
+    console.log(' original ', this.gamecard);
+  }
+
+  shuffleCards() {
+    for (let index = 0; index < 108; index++) {
+      let j = Math.floor(Math.random() * 108);
+      let swap = this.gamecard[j];
+      this.gamecard[j] = this.gamecard[index];
+      this.gamecard[index] = swap;
+    }
+    console.log(' shuffle ', this.gamecard);
+  }
+}  
