617d10a9ae1349d483e99753f2d666e412debfbb
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-21 15:33:02 +0100

    add rules

--- unoganesha/src/app/unocommon/header/header.component.ts
+++ unoganesha/src/app/unocommon/header/header.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
--- unoganesha/src/app/unogame/welcome/welcome.component.ts
+++ unoganesha/src/app/unogame/welcome/welcome.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ColorUno } from 'src/app/unocommon/Enum/color';
 import { FigureUno } from 'src/app/unocommon/Enum/figure';
+import { ICardUno } from 'src/app/unocommon/Model/carduno';
 
 @Component({
     selector: 'uno-welcome',
     templateUrl: './welcome.component.html',
     styleUrls: ['./welcome.component.scss'],
+    encapsulation: ViewEncapsulation.None,
     standalone: false
 })
 export class WelcomeComponent implements OnInit {
+  isRulesVisible = false;
   constructor() { }
+  unocolor: ColorUno = ColorUno.Blue;
+  card: ICardUno  = {figure: FigureUno.NUM1, color: ColorUno.Black,  showBack: false};
 
   ngOnInit(): void {
+  }
+
+  toggleRules() {
+    this.isRulesVisible = !this.isRulesVisible;
   }
 
 }
